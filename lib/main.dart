fc64a507732cff5568d1423666eee1b74ea2ac32
nguoidepxuhue <ngocungducanh2022@gmail.com>
2024-01-30 22:12:55 +0700

    tuan 7

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,9 +19,35 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProductListScreen(),
+      home: HomeScreen(),
+      // home: ProductListScreen(),
     );
   }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text("Trang chu"),
+      ),
+      body: Center(
+        child: ElevatedButton (
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProductListScreen()),
+            );
+          },
+          child: Text("Go to ProductListScreen"),
+        ),
+      ),
+    );
+
+  }
+
+
 }
 
 class ProductListScreen extends StatefulWidget {
@@ -72,10 +99,10 @@ class _ProductListScreenState extends State<ProductListScreen> {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("Danh sách sản phẩm"),
+        title: Text("Danh sach san pham"),
       ),
       body: products != null ?
-      ListView.builder( 
+      ListView.builder(
         itemCount: products.length,
         itemBuilder: (context, index) {
           return ListTile(
@@ -93,11 +120,75 @@ class _ProductListScreenState extends State<ProductListScreen> {
                 height: 50,
                 fit: BoxFit.cover
             ),
+            onTap: () {
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ProductDetailScreen(products[index])),
+              );
+            },
           );
         },
       ) : Center(
         child: CircularProgressIndicator(),
       ),
+    );
+  }
+}
+
+class ProductDetailScreen extends StatelessWidget {
+  final Product product;
+  ProductDetailScreen(this.product);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Detail'),
+        actions: [
+          ElevatedButton(onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+                builder: (context) => CartSceen()
+            ));
+          },
+              child: Icon(Icons.shopping_cart),
+              style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(0)
+              )
+          ),
+
+        ],
+
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(padding: const EdgeInsets.all(8),
+            child: Text('Nhanh ${product.brands_filter_facet}'),
+          ),
+          Image.network(product.search_image),
+          Padding(padding: const EdgeInsets.all(8),
+            child: Text(
+              product.product_additional_info,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(padding: const EdgeInsets.all(8),
+            child: Text('ID: ${product.styleid}'),
+          ),
+          Padding(padding: const EdgeInsets.all(8),
+            child: Text('Price: ${product.price}'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CartSceen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Shopping Cart"),),
+      body: Center(child: Text("Gio hang cua ban"),),
     );
   }
 }
